3638dc82907346af5257318b2c0ca90c37fba493
aloksoni21 <aloksoni2107@gmail.com>
2024-05-09 01:38:18 +0530

    Add files via upload
    Adding Paasword to the User model

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -14,6 +14,8 @@ public class User {
     private Long id;
     private String name;
     private String email;
+    private String password;
+    
 	public Long getId() {
 		return id;
 	}
@@ -31,6 +33,12 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	@Override
 	public String toString() {
